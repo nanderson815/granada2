518f7de26097e9ce3df399fd17160f50c7953b70
Noah Anderson <nanderson815@gmail.com>
2021-11-28 20:46:54 -0500

    removing func

--- src/components/sections/Contact.js
+++ src/components/sections/Contact.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import axios from 'axios';
 
 import { Section, Container } from '@components/global';
 import Input from '@common/Input';
